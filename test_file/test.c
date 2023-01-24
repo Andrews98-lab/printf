9426e6c2d64795f028a8e0276dd24de46a56f00c
Andrews98-lab <84232691+Andrews98-lab@users.noreply.github.com>
2023-01-24 17:50:26 +0000

    test files error fixed

--- test_file/test.c
+++ test_file/test.c
@@ -1,16 +1,16 @@
 #include <limits.h>
 #include <stdio.h>
 #include "main.h"
-/**
- *
- *  * main - Entry point
- *
- *    * Return: Always 0
- **/
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
 int main(void)
 {
-	  int n = _printf("%u", 2.35);
-	    printf("\nn = %d \n", n);
-	      return (0);
+  int n = _printf("%u", 2.35);
+  printf("\nn = %d \n", n);
+
+  return (0);
 }
